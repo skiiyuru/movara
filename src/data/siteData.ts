a80d89b450d6dc6291bd86d804dda304192bbb91
StephenKiiyuruNganga <steveknganga@gmail.com>
2025-07-05 13:21:21 +0300

    add base styles

---
+++ src/data/siteData.ts
@@ -0,0 +1,23 @@
+export const baseData = {
+  title: 'Movara Horizon',
+  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.`,
+  logo: {
+    src: '/favicon.svg',
+    alt: 'Movara Horizon',
+  },
+}
+
+export const socialData = [
+  {
+    href: 'https://www.linkedin.com/in/movara-horizon/',
+    icon: 'linkedin',
+    title: 'LinkedIn',
+  },
+  // {
+  //   href: '/rss.xml',
+  //   icon: 'rss',
+  //   title: 'RSS Feed',
+  // },
+]
+
+export const EMAIL = 'steveknganga@gmail.com'
